25d97da24e8c156256fbe3056ed2b4d84f156f0f
sdavrieux <sdavrieux@gmail.com>
2015-10-28 10:21:03 +0100

    DEV-02 - Creato il layout e il tipo MenuItem, creata la list menuitems.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/MenuActivity.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/MenuActivity.java
@@ -2,12 +2,41 @@ package it.hopapps.villaggiorock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.hopapps.villaggiorock.models.MenuItem;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private List<MenuItem> menuItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.recicler_view_activity_menu);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        initializeMenuItems();
+        setMenu(rv, llm);
+
+    }
+
+    private void setMenu (RecyclerView rv, LinearLayoutManager llm){
         setContentView(R.layout.activity_menu);
+        rv.setLayoutManager(llm);
+
+    }
+
+    private void initializeMenuItems(){
+        menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Events", R.drawable.menuItemEvents));
+        menuItems.add(new MenuItem("Foto", R.drawable.menuItemFoto));
+        menuItems.add(new MenuItem("Playlist", R.drawable.menuItemPlaylist));
+        menuItems.add(new MenuItem("Prenotazione", R.drawable.menuItemPrenotazione));
+        menuItems.add(new MenuItem("Gadget", R.drawable.menuItemGadget));
+        menuItems.add(new MenuItem("Live", R.drawable.menuItemLive));
     }
 }
---
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/models/MenuItem.java
@@ -0,0 +1,11 @@
+package it.hopapps.villaggiorock.models;
+
+public class MenuItem {
+    private String name;
+    private int photoId;
+
+    public MenuItem(String name, int photoId) {
+        this.name = name;
+        this.photoId = photoId;
+    }
+}
