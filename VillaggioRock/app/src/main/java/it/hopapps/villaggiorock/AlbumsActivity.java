5ecbcd49574705632fe95de10e85ad9f6b83965a
sdavrieux <sdavrieux@gmail.com>
2015-10-30 19:01:39 +0100

    [FIX-01] Changed layout from cardlayout to gridlayout.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/AlbumsActivity.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/AlbumsActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import it.hopapps.villaggiorock.adapters.AlbumsImageAdapter;
 import it.hopapps.villaggiorock.adapters.RVMenuAdapter;
 import it.hopapps.villaggiorock.models.MenuItem;
 
@@ -20,17 +25,22 @@ public class AlbumsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
         initializeMenuItems();
-        RecyclerView rv = (RecyclerView) findViewById(R.id.recicler_view_albums_menu);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        rv.setLayoutManager(llm);
-        RVMenuAdapter adapter = new RVMenuAdapter(albumsItems);
-        rv.setAdapter(adapter);
-        rv.setHasFixedSize(true);
+        GridView gridview = (GridView) findViewById(R.id.albums_grid_view);
+        gridview.setAdapter(new AlbumsImageAdapter(this, albumsItems));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(AlbumsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initializeMenuItems(){
         albumsItems = new ArrayList<>();
         albumsItems.add(new MenuItem("Album 1", R.drawable.menu_item_events));
         albumsItems.add(new MenuItem("Album 2", R.drawable.menu_item_foto));
+        albumsItems.add(new MenuItem("Album 3", R.drawable.menu_item_gadget));
+        albumsItems.add(new MenuItem("Album 4", R.drawable.menu_item_prenotazione));
+        albumsItems.add(new MenuItem("Album 5", R.drawable.menu_item_live));
     }
 }
---
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/adapters/AlbumsImageAdapter.java
@@ -0,0 +1,57 @@
+package it.hopapps.villaggiorock.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import it.hopapps.villaggiorock.R;
+import it.hopapps.villaggiorock.models.MenuItem;
+
+public class AlbumsImageAdapter extends BaseAdapter {
+    private Context context;
+    private List<MenuItem> albumsItems;
+
+    public AlbumsImageAdapter(Context context, List<MenuItem> albumsItems) {
+        this.context = context;
+        this.albumsItems = albumsItems;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gridView;
+        if (convertView == null) {
+            gridView = new View(context);
+            gridView = inflater.inflate(R.layout.albums_layout, null);
+            TextView textView = (TextView) gridView.findViewById(R.id.grid_albums_label);
+            textView.setText(albumsItems.get(position).getName());
+            ImageView imageView = (ImageView) gridView.findViewById(R.id.grid_albums_image);
+            imageView.setImageResource(albumsItems.get(position).getPhotoId());
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return albumsItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
