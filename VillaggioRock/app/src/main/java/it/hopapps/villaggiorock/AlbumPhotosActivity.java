190b668ba7ec53e075523aa1da95d16a0b7d8d83
sdavrieux <sdavrieux@gmail.com>
2015-10-30 19:16:16 +0100

    [DEV-07] Moved array of images from adapter to activity.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/AlbumPhotosActivity.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/AlbumPhotosActivity.java
@@ -17,7 +17,7 @@ public class AlbumPhotosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_album_photos);
 
         GridView gridview = (GridView) findViewById(R.id.album_photos_grid);
-        gridview.setAdapter(new ImageGridAdapter(this));
+        gridview.setAdapter(new ImageGridAdapter(this, mThumbIds));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
@@ -27,4 +27,18 @@ public class AlbumPhotosActivity extends AppCompatActivity {
             }
         });
     }
+
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
 }
--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/adapters/ImageGridAdapter.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/adapters/ImageGridAdapter.java
@@ -13,9 +13,11 @@ import it.hopapps.villaggiorock.R;
 
 public class ImageGridAdapter extends BaseAdapter {
     private Context mContext;
+    private Integer[] mThumbIds;
 
-    public ImageGridAdapter(Context c) {
+    public ImageGridAdapter(Context c, Integer[] mThumbIds) {
         mContext = c;
+        this.mThumbIds = mThumbIds;
     }
 
     public int getCount() {
@@ -43,34 +45,6 @@ public class ImageGridAdapter extends BaseAdapter {
         }
         return gridView;
 
-
-        /*ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;*/
     }
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
-    };
 }
