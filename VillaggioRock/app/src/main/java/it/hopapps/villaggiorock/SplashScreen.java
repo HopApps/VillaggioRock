9fe00b4ace8b94d879bbf77126809d1a2b2cc2ea
sdavrieux <sdavrieux@gmail.com>
2015-10-31 17:26:37 +0100

    DEV-10 - Added link between splash screen and menu activity.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/SplashScreen.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/SplashScreen.java
@@ -1,7 +1,11 @@
 package it.hopapps.villaggiorock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Button connectWithFacebookButton = (Button) findViewById(R.id.buttonLogin);
+        connectWithFacebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreen.this, MenuActivity.class);
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
     }
 }
