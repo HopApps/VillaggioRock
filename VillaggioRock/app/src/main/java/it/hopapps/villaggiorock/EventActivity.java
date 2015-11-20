c60026fbd6308c00bb0cecb8260644705fc426b6
sdavrieux <sdavrieux@gmail.com>
2015-11-20 18:23:59 +0100

    Solved crash on first rvsp_event allowance.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/EventActivity.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/EventActivity.java
@@ -3,25 +3,33 @@ package it.hopapps.villaggiorock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
 import java.util.Arrays;
 
 import it.hopapps.villaggiorock.asyncTasks.FacebookEventAttending;
 import it.hopapps.villaggiorock.asyncTasks.FacebookEventRetriever;
 
 public class EventActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    String eventId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
         Intent i = this.getIntent();
-        final String eventId = i.getExtras().getString("id");
+        eventId = i.getExtras().getString("id");
         FacebookEventRetriever fber = new FacebookEventRetriever(eventId, this);
         fber.execute();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -34,21 +42,24 @@ public class EventActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!AccessToken.getCurrentAccessToken().getPermissions().contains("rsvp_event")) {
                     LoginManager.getInstance().logInWithPublishPermissions(EventActivity.this, Arrays.asList("rsvp_event"));
-                    FacebookEventAttending fea = new FacebookEventAttending(eventId, fab, view);
-                    fea.execute();
                 } else {
                     FacebookEventAttending fea = new FacebookEventAttending(eventId, fab, view);
                     fea.execute();
                 }
-
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        FacebookEventAttending fea = new FacebookEventAttending(eventId, fab, this.findViewById(R.id.event_layout_coordinator));
+        fea.execute();
     }
 
 }
