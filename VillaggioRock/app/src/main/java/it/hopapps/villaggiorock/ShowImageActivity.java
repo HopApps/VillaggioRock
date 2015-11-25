73dfa0a731024258e95e63c4b38c6d35d88c0dc3
Francesco Trombi <trombi92@gmail.com>
2015-11-25 11:17:23 +0100

    [DEV-31] Cleaned ShowImageActivity

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/ShowImageActivity.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/ShowImageActivity.java
@@ -1,6 +1,5 @@
 package it.hopapps.villaggiorock;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +10,11 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class ShowImageActivity extends AppCompatActivity {
 
-    private DisplayImageOptions options;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        options = new DisplayImageOptions.Builder()
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
                 .considerExifParams(true)
@@ -24,10 +22,12 @@ public class ShowImageActivity extends AppCompatActivity {
                 .build();
         setContentView(R.layout.activity_show_image);
 
-        Intent i = this.getIntent();
-        String eventId = i.getExtras().getString("id");
+        String eventId = this.getIntent().getExtras().getString("id");
 
-        ImageView img = (ImageView) findViewById(R.id.fullscreen_photo_image_view);
-        ImageLoader.getInstance().displayImage(eventId, img, options);
+        ImageLoader.getInstance().displayImage(
+                eventId,
+                (ImageView) findViewById(R.id.fullscreen_photo_image_view),
+                options
+        );
     }
 }
