75e537e9a0d0f2d9edb329ff6428f47a7a8ccfda
Francesco Trombi <trombi92@gmail.com>
2015-11-25 11:31:30 +0100

    [DEV-31] Cleaned SplashScreen. Added login_failed and login_canceled strings.

--- VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/SplashScreen.java
+++ VillaggioRock/app/src/main/java/it/hopapps/villaggiorock/SplashScreen.java
@@ -9,40 +9,38 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class SplashScreen extends AppCompatActivity {
-    CallbackManager callbackManager;
-    LoginButton connectWithFacebookButton;
-    List permissions = Arrays.asList("email");
+    private CallbackManager callbackManager;
+    private List permissions = Collections.singletonList("email");
 
     FacebookCallback fbCallback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
-            //Intent intent = new Intent(SplashScreen.this, MenuActivity.class);
-            //intent.putExtra(EXTRA_MESSAGE, message);
-            //startActivity(intent);
-            //Snackbar.make(findViewById(android.R.id.content), loginResult.getAccessToken().getUserId(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
 
         @Override
         public void onCancel() {
-            Snackbar.make(findViewById(android.R.id.content), "Login attempt canceled.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Snackbar.make(findViewById(android.R.id.content), getString(R.string.splashscreen_login_canceled), Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
 
         @Override
         public void onError(FacebookException exception) {
-            Snackbar.make(findViewById(android.R.id.content), "Login attempt failed.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Snackbar.make(findViewById(android.R.id.content), getString(R.string.splashscreen_login_failed), Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_splash_screen);
-        connectWithFacebookButton = (LoginButton) findViewById(R.id.buttonLogin);
+
+        LoginButton connectWithFacebookButton = (LoginButton) findViewById(R.id.buttonLogin);
         connectWithFacebookButton.setReadPermissions(permissions);
         connectWithFacebookButton.registerCallback(callbackManager, fbCallback);
     }
